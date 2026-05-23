fbfffe369ed11e4bd6ed111ac0e498c6f3fadd57
Angel Santiago <lsantiago@miumg.edu.gt>
2026-05-22 23:12:38 -0600

    feat(mongo): agregar repositorio y servicio para persistencia MongoDB Integrante C

--- tree-app/src/main/java/gt/edu/umg/repository/MongoNodeRepository.java
+++ tree-app/src/main/java/gt/edu/umg/repository/MongoNodeRepository.java
@@ -4,7 +4,15 @@ import gt.edu.umg.model.MongoNode;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MongoNodeRepository extends MongoRepository<MongoNode, String> {
-    // Spring Data Mongo nos da los métodos básicos sin escribir código extra
+    
+    // 1. Busca todos los nodos hijos de una categoría específica
+    List<MongoNode> findByParentId(String parentId);
+    
+    // 2. Busca la categoría principal o "Raíz" (la única que no tiene padre)
+    Optional<MongoNode> findByParentIdIsNull();
 }
---
+++ tree-app/src/main/java/gt/edu/umg/service/MongoTreeService.java
@@ -0,0 +1,5 @@
+package gt.edu.umg.service;
+
+public class MongoTreeService {
+
+}
